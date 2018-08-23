5917f83d9a4f554a8cab93f2b7c938a8056ffcac
Juhezi <juhezix@163.com>
2018-08-23 11:21:55 +0800

    [Update] getRemaining function

--- time/src/main/java/me/juhezi/time/TimeActivity.kt
+++ time/src/main/java/me/juhezi/time/TimeActivity.kt
@@ -4,19 +4,23 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_time.*
 import me.juhezi.eternal.base.BaseActivity
 import me.juhezi.eternal.extension.i
-import me.juhezi.eternal.global.formatTime
 import java.util.*
 
 class TimeActivity : BaseActivity() {
 
     private var timer: Timer? = null
-    private val destination: Date = Date(2018 - 1900, 11, 22)
+    private val destination: Calendar = Calendar.getInstance()
 
+
+    // 2018 11 22
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time)
         toolBarVisibility = false
-        i(destination.toGMTString())
+        destination.time = Date(2018 - 1900, 11, 22)
+        i("${destination[Calendar.YEAR]}\t${destination[Calendar.MONTH]}" +
+                "\t${destination[Calendar.DAY_OF_MONTH]}\t${destination[Calendar.HOUR_OF_DAY]}" +
+                "\t${destination[Calendar.MINUTE]}\t${destination[Calendar.SECOND]}")
     }
 
     override fun onResume() {
@@ -32,11 +36,20 @@ class TimeActivity : BaseActivity() {
     }
 
     /**
+     * todo 下沉到基础模块
      * 计算剩余时间的函数
      */
     private fun getRemaining(): String {
-        val diff = destination.time - System.currentTimeMillis()
-        return formatTime(diff, "D 天 HH:mm:ss")
+        val current = Calendar.getInstance()
+        val diff = destination.timeInMillis - current.timeInMillis
+        val day = diff / (1000 * 60 * 60 * 24)
+        val remindTimeForHour = diff % (1000 * 60 * 60 * 24)
+        val hour = remindTimeForHour / (1000 * 60 * 60)
+        val remindTimeForMinute = remindTimeForHour % (1000 * 60 * 60)
+        val minute = remindTimeForMinute / (1000 * 60)
+        val remindTimeForSecond = remindTimeForMinute % (1000 * 60)
+        val second = remindTimeForSecond / 1000
+        return "$day 天 $hour:$minute:$second"
     }
 
     private fun startTimer() {
