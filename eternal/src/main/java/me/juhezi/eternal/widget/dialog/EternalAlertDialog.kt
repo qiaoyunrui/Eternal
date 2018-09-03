89d7f3e731429e9ac61f54ba53b3756fa5b63bed
LiYitiao <juhezix@163.com>
2018-09-03 23:01:49 +0800

    [Update]

---
+++ eternal/src/main/java/me/juhezi/eternal/widget/dialog/EternalAlertDialog.kt
@@ -0,0 +1,13 @@
+package me.juhezi.eternal.widget.dialog
+
+import android.content.Context
+import me.juhezi.eternal.R
+import me.juhezi.eternal.base.BaseDialog
+
+/**
+ * Created by Juhezi[juhezix@163.com] on 2018/9/3.
+ */
+class EternalAlertDialog(context: Context?, theme: Int = R.style.full_screen_dialog) :
+        BaseDialog(context, theme) {
+    
+}
