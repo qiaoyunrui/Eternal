2d9853793e258b6c1f7ffad5ba854494bdc55c82
Juhezi <juhezix@163.com>
2018-09-04 19:39:05 +0800

    [Update] start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-./gradlew :notepad:assembleDebug &&
 ./gradlew :notepad:installDebug &&
 adb shell am start -n "me.juhezi.eternal.notepad/me.juhezi.notepad.main.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER &&
 say "Nice to meet you!" &&
