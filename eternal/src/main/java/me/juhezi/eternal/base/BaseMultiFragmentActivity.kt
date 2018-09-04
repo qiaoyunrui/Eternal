296831a01efb67a61d69a4bd269a4ebaa8921fa9
LiYitiao <juhezix@163.com>
2018-09-04 22:55:13 +0800

    [Update] BaseMultiFragmentActivity

--- eternal/src/main/java/me/juhezi/eternal/base/BaseMultiFragmentActivity.kt
+++ eternal/src/main/java/me/juhezi/eternal/base/BaseMultiFragmentActivity.kt
@@ -3,8 +3,10 @@ package me.juhezi.eternal.base
 import android.os.Bundle
 import android.support.annotation.IdRes
 import android.support.annotation.LayoutRes
+import android.text.TextUtils
 import me.juhezi.eternal.R
 import java.util.*
+import kotlin.collections.HashMap
 
 /**
  * 盛放多个 Fragment 的 Activity 基类
@@ -12,7 +14,9 @@ import java.util.*
  */
 abstract class BaseMultiFragmentActivity : BaseActivity() {
 
+    // Fragment 的返回栈
     private val mFragmentStack = Stack<BaseFragment>()
+    private val mFragmentMap: MutableMap<String, BaseFragment> = HashMap()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,10 +37,53 @@ abstract class BaseMultiFragmentActivity : BaseActivity() {
             }
 
     /**
-     * 移除当前显示的 Fragment
+     * 添加一个 Fragment
      */
-    private fun removeFragment() {
+    protected fun add(key: String?, fragment: BaseFragment): String {
+        var internalKey = key
+        if (TextUtils.isEmpty(key)) {
+            internalKey = System.currentTimeMillis().toString()
+        }
+        mFragmentMap[internalKey!!] = fragment
+        return internalKey
+    }
 
+    /**
+     * 删除一个 Fragment
+     */
+    protected fun remove(key: String) {
+        mFragmentMap.remove(key)
+    }
+
+    protected fun showFragment(key: String) {
+        val fragment = mFragmentMap[key] ?: return
+        showFragment(fragment)
+    }
+
+    protected fun showFragment(fragment: BaseFragment) {
+        mFragmentStack.push(fragment)
+        internalShowFragment(fragment)
+    }
+
+    private fun internalShowFragment(fragment: BaseFragment) {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(getFragmentContainerId(), fragment)
+                .commit()
+    }
+
+    protected fun hideFragment(): BaseFragment {
+        val currentFragment = mFragmentStack.pop()
+        if (mFragmentStack.empty()) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .remove(currentFragment)
+                    .commit()
+        } else {
+            val preFragment = mFragmentStack.peek()
+            internalShowFragment(preFragment)
+        }
+        return currentFragment
     }
 
     /**
@@ -47,11 +94,11 @@ abstract class BaseMultiFragmentActivity : BaseActivity() {
     open fun handleBackPressed(): Boolean {
         // 默认逻辑
         // 如果当前 fragment 栈里只有一个 fragment 的时候，不进行拦截，执行 Activity 的返回操作
-        if (mFragmentStack.size > 1) {
-            removeFragment()
-            return true
+        return if (mFragmentStack.size > 1) {
+            hideFragment()
+            true
         } else {
-            return false
+            false
         }
     }
 
@@ -65,5 +112,5 @@ abstract class BaseMultiFragmentActivity : BaseActivity() {
         }
         super.onBackPressed()
     }
-    
+
 }
