cffe42714426f17f00bf645f5042e16b47d7da5a
Juhezi <juhezix@163.com>
2018-11-05 19:39:04 +0800

    [Add] AccompanyDecoderController

--- orange/src/main/cpp/ffmpeg_decoder/accompany_decoder.hpp
+++ orange/src/main/cpp/ffmpeg_decoder/accompany_decoder.hpp
@@ -21,8 +21,6 @@
 #define INT64_MAX	9223372036854775807LL
 #endif
 
-#endif //ETERNAL_ACCOMPANY_DECODER_HPP
-
 typedef struct AudioPacket {
     static const int AUDIO_PACKET_ACTION_PLAY = 0;
     static const int AUDIO_PACKET_ACTION_PAUSE = 100;
@@ -54,7 +52,7 @@ typedef struct AudioPacket {
 
 } AudioPacket;
 
-#include "common/CommonTools.h"
+#include "../common/CommonTools.h"
 
 extern "C" {
 #include "libavcodec/avcodec.h"
@@ -87,4 +85,80 @@ private:
     float timeBase;
     AVFrame *pAudioFrame;
     AVPacket packet;
+
+    char *accompanyFilePath;
+    bool seek_success_read_frame_success;
+    int packetBufferSize;
+
+    // 每次解码出来的 AudioBuffer 以及这个 AudioBuffer 的时间戳以及当前这个类对于这个 AudioBuffer 的操作情况
+    short *audioBuffer;
+    float position;
+    int audioBufferCursor;
+    int audioBufferSize;
+    float duration;
+    bool isNeedFirstFrameCorrectFlag;
+    float firstFrameCorrectionInSecs;
+
+    SwrContext *swrContext;
+    void *swrBuffer;
+    int swrBufferSize;
+
+    int init(const char *fileString);
+
+    int readSamples(short *samples, int size);
+
+    int readFrame();
+
+    bool audioCodecIsSupported();
+
+public:
+    AccompanyDecoder();
+
+    virtual ~AccompanyDecoder();
+
+    // 获取采样率以及比特率
+    virtual int getMusicMeta(const char *fileString, int *metaData);
+
+    // 初始化这个 decoder，即打开指定的 mp3 文件
+    virtual void init(const char *fileString, int packetBufferSizeParam);
+
+    virtual AudioPacket *decodePacket();
+
+    // 销毁这个 decoder
+    virtual void destroy();
+
+    void setSeekReq(bool seekReqParam) {
+        seek_req = seekReqParam;
+        if (seek_req) {
+            seek_resp = false;
+        }
+    };
+
+    bool hasSeekReq() {
+        return seek_req;
+    };
+
+    bool hasSeekResp() {
+        return seek_resp;
+    };
+
+
+    void setPosition(float seconds) {
+        actualSeekPosition = -1;
+        this->seek_seconds = seconds;
+        this->seek_req = true;
+        this->seek_resp = false;
+    };
+
+    float getActualSeekPosition() {
+        float ret = actualSeekPosition;
+        if (ret != -1) {
+            actualSeekPosition = -1;
+        }
+        return ret;
+    }
+
+    virtual void seek_frame();
 };
+
+#endif //ETERNAL_ACCOMPANY_DECODER_HPP
